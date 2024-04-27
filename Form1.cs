91a050ce89edd4187b48c9dc21035c44c4a05e02
Paolocute <159393828+Paolocute@users.noreply.github.com>
2024-04-28 05:37:14 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,93 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Collections;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace StudentRegistrationForm
 {
     public partial class Form1 : Form
     {
-        private object i;
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-            double LastName, FirstName, MiddleName;
-
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        public void DisplayInfo(string LastName, string FirstName, string MiddleName, string Gender, string Month, string Day, string Year, string Program)
         {
-
+            MessageBox.Show("Student Name: " + LastName + ", " + FirstName + " " + MiddleName + "\nGender: " + Gender +
+          "\nDate of Birth: " + Month + "/" + Day + "/" + Year + "\nProgram: " + Program);
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        public void DisplayInfo(string LastName, string FirstName, string MiddleName, string Gender, string Program)
         {
-
+            MessageBox.Show("Student Name: " + LastName + ", " + FirstName + " " + MiddleName + "\nGender: " + Gender + "\nProgram: " + Program);
         }
 
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        public void DisplayInfo(string LastName, string FirstName, string MiddleName, string Program)
         {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void B1_Click(object sender, EventArgs e)
-        {
-           string MM = comboBox1.GetItemText(comboBox1.SelectedItem);
-           string DD = comboBox2.GetItemText(comboBox2.SelectedItem);
-           string YYYY = comboBox3.GetItemText(comboBox3.SelectedItem);
-           string gender = checkBox1.Checked == true ? "Male" : "Female";
-            
-            string LastName, FirstName, MiddleName;
-
-            LastName = T1.Text;
-            FirstName = T2.Text;
-            MiddleName = T3.Text;
-
-            MessageBox.Show("Student Name: " + FirstName + " " + MiddleName + " " + LastName + "\nGender: " + gender + "\nDate of Birth: " + DD + "/" + MM + "/" + YYYY);
+            MessageBox.Show("Student Name: " + LastName + ", " + FirstName + " " + MiddleName + "\nProgram: " + Program);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-        }
-
-
-        private void Form1_Load_1(object sender, EventArgs e)
-        {
-            for(int i = 0; i < 31; i++)
+            for (int i = 1; i < 32; i++)
             {
                 comboBox1.Items.Add(i.ToString());
             }
-            for(int i = 1;i <=12; i++)
+            ArrayList monthList = new ArrayList();
+            monthList.Add(" January ");
+            monthList.Add(" February ");
+            monthList.Add("March");
+            monthList.Add("April");
+            monthList.Add("May");
+            monthList.Add("June");
+            monthList.Add("July");
+            monthList.Add("August");
+            monthList.Add("September");
+            monthList.Add("October");
+            monthList.Add("November");
+            monthList.Add("December");
+
+            foreach (string month in monthList)
             {
-                comboBox2.Items.Add(i.ToString());
+                comboBox2.Items.Add(month);
             }
-            for(int i = 1900; i <= 2024; i++)
+
+            for (int i = 1900; i <= 2024; i++)
             {
                 comboBox3.Items.Add(i.ToString());
             }
+
+            ArrayList programList = new ArrayList();
+            programList.Add("Bachelor of Science in Information Technology");
+            programList.Add("Bachelor of Science in Computer Science");
+            programList.Add("Bachelor of Science in Information Systems");
+            programList.Add("Bachelor of Science in Computer Engineering");
+
+            foreach (string program in programList)
+            {
+                comboBox4.Items.Add(program);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string month = comboBox1.GetItemText(comboBox1.SelectedItem);
+            string DD = comboBox2.GetItemText(comboBox2.SelectedItem);
+            string YYYY = comboBox3.GetItemText(comboBox3.SelectedItem);
+            string gender = radioButton1.Checked == true ? "Male" : "Female";
+            string program = comboBox4.GetItemText(comboBox4.SelectedItem);
+            string LastName, FirstName, MiddleName;
+            LastName = textBox1.Text;
+            FirstName = textBox2.Text;
+            MiddleName = textBox3.Text;
+
+            DisplayInfo(LastName, FirstName, MiddleName, gender, month, DD, YYYY, program);
+            DisplayInfo(LastName, FirstName, MiddleName, gender, program);
+            DisplayInfo(LastName, FirstName, MiddleName, program);
+
+            openFileDialog1.ShowDialog();
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
         }
     }
-  
 }
+
